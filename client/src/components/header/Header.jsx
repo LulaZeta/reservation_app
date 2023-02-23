d87f61f1476ae1d01734569ca6378cca56dd48a1
Luciana Zanetti <luciana.zeta@gmail.com>
2023-02-23 11:31:14 -0300

    user

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -16,6 +16,7 @@ import { format } from 'date-fns';
 import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SearchContext } from '../../context/SearchContext';
+import { AuthContext } from '../../context/AuthContext';
 
 const Header = ({ type }) => {
   const [destination, setDestination] = useState('');
@@ -35,6 +36,8 @@ const Header = ({ type }) => {
     room: 1,
   });
   const navigate = useNavigate();
+
+  const { user } = useContext(AuthContext);
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -90,7 +93,7 @@ const Header = ({ type }) => {
               Get rewarded for your travels – unlock instant savings of 10% or
               more with a free Lulabooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
