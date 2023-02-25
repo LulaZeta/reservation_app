e1e4d86a7f1b556f57b924e514503225372e6ed1
Luciana Zanetti <luciana.zeta@gmail.com>
2023-02-25 08:36:22 -0300

    users list

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -1,25 +1,26 @@
-import "./datatable.scss";
-import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import './datatable.scss';
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns } from '../../datatablesource';
+import { Link } from 'react-router-dom';
+//import { useState } from 'react';
+import useFetch from '../../hooks/useFetch';
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const { data, loading, error } = useFetch('/users');
 
   const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+    //setData(data.filter((item) => item.id !== id));
   };
 
   const actionColumn = [
     {
-      field: "action",
-      headerName: "Action",
+      field: 'action',
+      headerName: 'Action',
       width: 200,
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
+            <Link to="/users/test" style={{ textDecoration: 'none' }}>
               <div className="viewButton">View</div>
             </Link>
             <div
@@ -48,6 +49,7 @@ const Datatable = () => {
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+        getRowId={(row) => row._id}
       />
     </div>
   );
