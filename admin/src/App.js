55378914d1559d413b189757a01e8a66903e9f6d
Luciana Zanetti <luciana.zeta@gmail.com>
2023-02-25 09:19:13 -0300

    me voy al club

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,6 +9,7 @@ import './style/dark.scss';
 import { useContext } from 'react';
 import { DarkModeContext } from './context/darkModeContext';
 import { AuthContext } from './context/AuthContext';
+import { hotelColumns, roomColumns, userColumns } from './datatablesource';
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -40,7 +41,7 @@ function App() {
                 index
                 element={
                   <ProtectedRoute>
-                    <List />
+                    <List columns={userColumns} />
                   </ProtectedRoute>
                 }
               />
