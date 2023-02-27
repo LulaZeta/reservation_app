4c7c5fb0b8144ed06c5817492cffcc0a2c243764
Luciana Zanetti <luciana.zeta@gmail.com>
2023-02-27 06:58:20 -0300

    New Room

--- admin/src/App.js
+++ admin/src/App.js
@@ -11,6 +11,7 @@ import { DarkModeContext } from './context/darkModeContext';
 import { AuthContext } from './context/AuthContext';
 import { hotelColumns, roomColumns, userColumns } from './datatablesource';
 import NewHotel from './pages/newHotel/NewHotel';
+import NewRoom from './pages/newRoom/NewRoom';
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -78,6 +79,32 @@ function App() {
                 element={
                   <ProtectedRoute>
                     <NewHotel />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path="rooms">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List columns={roomColumns} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path=":productId"
+                element={
+                  <ProtectedRoute>
+                    <Single />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectedRoute>
+                    <NewRoom />
                   </ProtectedRoute>
                 }
               />
